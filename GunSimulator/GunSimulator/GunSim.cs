aab1476ec99cbdec08b7185098ec3c6470e58ae6
Camal <90691266+JamalZl@users.noreply.github.com>
2021-10-05 11:44:29 +0400

    Add files via upload

---
+++ GunSimulator/GunSimulator/GunSim.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GunSimulator
+{
+    class GunSim
+    {
+        private int numofbullets;
+
+        public GunSim(int bullets)
+        {
+            numofbullets = bullets;
+            Console.WriteLine("**Let's start shooting**");
+        }
+        public void PistolCharge()
+        {
+            if (numofbullets == 15)
+            {
+                Console.WriteLine("Charger is fulled");
+            }
+            else if (numofbullets < 15)
+            {
+                Console.WriteLine("Fill the charger!!!");
+
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Charger cannot take more than 15 bullets!!!");
+                return;
+            }
+            Console.WriteLine("Number of bullets:");
+            Console.WriteLine(numofbullets);
+        }
+        public void PistolShut()
+        {
+            for (int i = 6; i >= 0; i--)//Tutaq ki,6 maqazin var ve her maqazinin gulle tutumu 15-dir.
+            {
+                if (numofbullets < 15 || numofbullets > 15)
+                {
+                    return;
+                }
+                else
+                {
+                    for (int j = numofbullets; j >= 0; j--)
+                    {
+                        if (j == 0)
+                        {
+                            Console.WriteLine($"{j}{" bullet left in the charger"}");
+                            Console.WriteLine("Pistol need to be recharged:");
+                            PistolCharge();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("There is no charger left anymore");
+            return;
+        }
+    }
+        
+} 
---
+++ GunSimulator/GunSimulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GunSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GunSim gs = new GunSim(15);
+            gs.PistolCharge();
+            gs.PistolShut(); 
+            //gs.PistolCharge();
+
+        }   
+    }
+}
